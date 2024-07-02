333ec1b0bda13a1574723e12f5c3cab92880ca9e
calu <chimit0412@gmail.com>
2024-07-02 22:32:38 +0700

    number guessing game

--- api/practice/server.js
+++ api/practice/server.js
@@ -8,9 +8,37 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/number', (req, res) => {});
+app.get('/api/number', (req, res) => {
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  res.json(
+    {
+      "message": "Số ngẫu hứng đã được tạo ra. Đoán xem, đấy là số mấy!"
+    }
+  )
+});
 
-app.post('/api/guess', (req, res) => {});
+app.post('/api/guess', (req, res) => {
+  const guess = req.body.guess;
+  if (guess > randomNumber) {
+      res.json(
+        {
+          "message": "Cao quá bây!"
+        }
+      )
+  } else if (guess < randomNumber) {
+    res.json(
+      {
+        "message": "Bé quá bây!"
+      }
+    )
+  } else {
+    res.json(
+      {
+        "message": "JUAN KO CẦN CHỈNH!"
+      }
+    )
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
