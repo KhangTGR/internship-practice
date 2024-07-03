870e329f324cbfd4bc25abfaf1688efcd81e12c9
calu <chimit0412@gmail.com>
2024-07-03 22:48:36 +0700

    log in and verify

--- api/ex2/practice/server.js
+++ api/ex2/practice/server.js
@@ -1,16 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const randomstring = require("randomstring");
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.use(bodyParser.json());
 
 // Hardcoded list of phone numbers
-const phoneNumbers = [];
+const phoneNumbers = ["836472473", "9133338767", "4967856248", "6043659811"];
+const otpCache = {};
 
-// API endpoint for `/login`
-// ...
+app.post("/login", (req, res) => {
+  const phone = req.body.phoneNumber;
+  const genotp = randomstring.generate({
+    length: 6,
+    charset: "numeric",
+  });
+  if (phoneNumbers.includes(phone)) {
+    res.json({
+      otp: genotp,
+      message: "Enter your OTP",
+    });
+    otpCache[phone] = genotp;
+  } else {
+    res.json({
+      error: "Phone number not registered",
+      message: "Phone number not registered, please try again.",
+    });
+  }
+});
+
+app.post("/verify", (req, res) => {
+  const phone = req.body.phoneNumber;
+  const otp = req.body.otp;
+  if (otpCache[phone] && otp == otpCache[phone]) {
+    res.json({
+      message: "Login successful!",
+    });
+  } else {
+    res.json({
+      error: "Invalid OTP or phone number",
+      message: "Invalid OTP",
+    });
+  }
+  delete otpCache[phone];
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
